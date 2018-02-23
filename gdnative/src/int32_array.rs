44dd4e71bf86801a3421c1ef4e2abcd80c2e7617
memoryruins <memoryruinsmusic@gmail.com>
2018-02-23 00:21:03 -0500

    add to int32_array docs

--- gdnative/src/int32_array.rs
+++ gdnative/src/int32_array.rs
@@ -4,11 +4,16 @@ use Variant;
 use GodotType;
 use VariantArray;
 
+/// A vector of `i32` that uses Godot's pool allocator.
 pub struct Int32Array(pub(crate) sys::godot_pool_int_array);
 
 impl Int32Array {
+    /// Creates an empty `Int32Array`.
     pub fn new() -> Self { Int32Array::default() }
 
+    /// Creates an array by trying to convert each variant
+    ///
+    /// See `Variant::to_int32_array`.
     pub fn from_variant_array(array: &VariantArray) -> Self {
         unsafe {
             let mut result = sys::godot_pool_int_array::default();
@@ -17,12 +22,14 @@ impl Int32Array {
         }
     }
 
+    /// Appends an element at the end of the array
     pub fn push(&mut self, val: i32) {
         unsafe {
             (get_api().godot_pool_int_array_append)(&mut self.0, val);
         }
     }
 
+    /// Appends an `Int32Array` at the end of this array.
     pub fn push_array(&mut self, array: &Int32Array) {
         unsafe {
             (get_api().godot_pool_int_array_append_array)(&mut self.0, &array.0);
@@ -30,6 +37,7 @@ impl Int32Array {
     }
 
     // TODO(error handling)
+    /// Insert a new int at a given position in the array.
     pub fn insert(&mut self, offset: i32, val: i32) -> bool {
         unsafe {
             let status = (get_api().godot_pool_int_array_insert)(&mut self.0, offset, val);
@@ -37,36 +45,42 @@ impl Int32Array {
         }
     }
 
+    /// Inverts the order of the elements in the array.
     pub fn invert(&mut self) {
         unsafe {
             (get_api().godot_pool_int_array_invert)(&mut self.0)
         }
     }
 
+    /// Removes an element at the given offset.
     pub fn remove(&mut self, idx: i32) {
         unsafe {
             (get_api().godot_pool_int_array_remove)(&mut self.0, idx);
         }
     }
 
+    /// Changes the size of the array, possibly removing elements or pushing default values.
     pub fn resize(&mut self, size: i32) {
         unsafe {
             (get_api().godot_pool_int_array_resize)(&mut self.0, size);
         }
     }
 
+    /// Gets a copy of the element at the given offset.
     pub fn get(&self, idx: i32) -> i32 {
         unsafe {
             (get_api().godot_pool_int_array_get)(&self.0, idx)
         }
     }
 
+    /// Sets the value of the element at the given offset.
     pub fn set(&mut self, idx: i32, val: i32) {
         unsafe {
             (get_api().godot_pool_int_array_set)(&mut self.0, idx, val);
         }
     }
 
+    /// Returns the number of elements in the array.
     pub fn len(&self) -> i32 {
         unsafe {
             (get_api().godot_pool_int_array_size)(&self.0)
