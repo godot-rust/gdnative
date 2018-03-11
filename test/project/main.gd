9471aa1a54ecd3a1b789f9489690a36fa76dfb36
Nicolas Silva <nical@fastmail.com>
2018-03-11 16:00:44 +0100

    Exit in-egnine tests with non-zero status when failing to find the shared library.

--- test/project/main.gd
+++ test/project/main.gd
@@ -3,16 +3,21 @@ extends Node
 func _ready():
     print(" -- Rust gdnative test suite:")
     var gdn = GDNative.new()
+    var status = false;
+
     gdn.library = load("res://gdnative.gdnlib")
+
     if gdn.initialize():
-        var status = gdn.call_native("standard_varcall", "run_tests", [])
+        status = gdn.call_native("standard_varcall", "run_tests", [])
         gdn.terminate()
-        if status:
-            print(" -- Test run completed successfully.")
-        else:
-            print(" -- Test run completed with errors.")
-            OS.exit_code = 1
     else:
         print(" -- Could not load the gdnative library.")
+
+    if status:
+        print(" -- Test run completed successfully.")
+    else:
+        print(" -- Test run completed with errors.")
+        OS.exit_code = 1
+
     print(" -- exiting.")
     get_tree().quit()
