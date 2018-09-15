f227e8617539e41c3b2a386857672805ea52a82c
Nicolas Silva <nical@fastmail.com>
2018-09-15 14:38:25 +0200

    Reintroduce casts between NativeScript and rust classes.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -9,3 +9,27 @@ use std::sync::{Once, ONCE_INIT};
 use std::ops::*;
 
 include!(concat!(env!("OUT_DIR"), "/common_types.rs"));
+
+impl NativeScript {
+    /// Try to down-cast from a `NativeScript` reference.
+    pub fn to_rust_script<T: NativeClass>(&self) -> Option<NativeRef<T>> {
+        unsafe {
+            // TODO: There's gotta be a better way.
+            let class = self.get_class_name();
+            let gd_name = GodotString::from_str(T::class_name());
+
+            if class != gd_name {
+                return None;
+            }
+
+            return Some(NativeRef::from_sys(self.this));
+        }
+    }
+
+    /// Up-cast to a `NativeScript` reference.
+    pub fn from_rust_script<T: NativeClass>(script: NativeRef<T>) -> NativeScript {
+        unsafe {
+            NativeScript::from_sys(script.sys())
+        }
+    }
+}
