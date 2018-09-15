5fa8cea2abbcd5531640c189c5f96dfa0a46d4a7
Nicolas Silva <nical@fastmail.com>
2018-09-15 13:12:25 +0200

    Lower case variants for the crate names.

--- graphics/build.rs
+++ graphics/build.rs
@@ -13,6 +13,6 @@ fn main() {
         File::open("../bindings_generator/api.json").unwrap(),
         File::open("../bindings_generator/namespaces.json").unwrap(),
         &mut output,
-        Crate::Graphics,
+        Crate::graphics,
     ).unwrap();
 }
