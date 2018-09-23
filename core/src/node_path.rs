293d3cc0f8f769a4ac8287b92360cdc1c5d1f7ad
Nicolas Silva <nical@fastmail.com>
2018-09-23 19:52:24 +0200

    Make NodePath interoperate with strings tad more nicely.

--- core/src/node_path.rs
+++ core/src/node_path.rs
@@ -15,10 +15,12 @@ use std::fmt;
 /// otherwise the last element will be used as a property name.
 ///
 /// If a string is passed to `Node.get_node()`, it will be automatically converted to a `NodePath`,
-/// but `NodePath` can be parsed ahead of time with `NodePath::from_str()` or `NodePath::from_godot_string()`.
+/// but `NodePath` can be parsed ahead of time with `NodePath::from_str` or `NodePath::new`.
 ///
-/// A `NodePath` consists of node names, “subnode” (resource) names, and the name of a property in
+/// A `NodePath` consists of node names, “sub-node” (resource) names, and the name of a property in
 /// the final node or resource.
+///
+/// More info at [Godot's official documentation](https://godot.readthedocs.io/en/3.0/classes/class_nodepath.html)
 pub struct NodePath(pub(crate) sys::godot_node_path);
 
 impl NodePath {
@@ -38,7 +40,7 @@ impl NodePath {
     }
 
     /// Create a `NodePath` from a GodotString.
-    pub fn from_godot_string(path: &GodotString) -> Self {
+    pub fn new(path: &GodotString) -> Self {
         unsafe {
             let mut dest = sys::godot_node_path::default();
             (get_api().godot_node_path_new)(&mut dest, &path.0);
@@ -60,7 +62,7 @@ impl NodePath {
         }
     }
 
-    // TODO DOC
+    /// Get the number of node names which make up the path.
     pub fn name_count(&mut self) -> i32 {
         unsafe {
             (get_api().godot_node_path_get_name_count)(&mut self.0)
@@ -68,20 +70,20 @@ impl NodePath {
     }
 
     /// Returns the resource name of the specified `idx`, 0 to subname_count()
-    pub fn subname(&self, idx: i32) -> GodotString {
+    pub fn get_subname(&self, idx: i32) -> GodotString {
         unsafe {
             GodotString((get_api().godot_node_path_get_subname)(&self.0, idx))
         }
     }
 
     /// Returns the number of resource names in the path.
-    pub fn subname_count(&self) -> i32 {
+    pub fn get_subname_count(&self) -> i32 {
         unsafe {
             (get_api().godot_node_path_get_subname_count)(&self.0)
         }
     }
 
-    pub fn concatenated_subnames(&self) -> GodotString {
+    pub fn get_concatenated_subnames(&self) -> GodotString {
         unsafe {
             GodotString((get_api().godot_node_path_get_concatenated_subnames)(&self.0))
         }
@@ -113,6 +115,22 @@ impl NodePath {
         /// Creates a new reference to this node path.
         pub fn new_ref(&self) -> NodePath : godot_node_path_new_copy;
     }
+}
+
+impl<S> From<S> for NodePath where S: AsRef<str> {
+    fn from(s: S) -> NodePath { NodePath::from_str(&s.as_ref()) }
+}
+
+impl Into<String> for NodePath {
+    fn into(self) -> String { self.to_string() }
+}
+
+impl From<GodotString> for NodePath {
+    fn from(s: GodotString) -> NodePath { NodePath::new(&s) }
+}
+
+impl Into<GodotString> for NodePath {
+    fn into(self) -> GodotString { self.to_godot_string() }
 }
 
 impl_basic_traits!(
