f6a371ecbdb1bb9cdca3b249668f5b7e250f0b8c
toasteater <48371905+toasteater@users.noreply.github.com>
2019-09-01 07:46:28 +0000

    Add tests for vector transmutation sanity
    Closes #184.

--- gdnative-core/src/vector2.rs
+++ gdnative-core/src/vector2.rs
@@ -17,17 +17,48 @@ impl FromVariant for Vector2 {
 
 godot_test!(
     test_vector2_variants {
-        let vector = Vector2::new(1.0, 2.0);
-        let variant = vector.to_variant();
-        let vector_from_variant = Vector2::from_variant(&variant).unwrap();
+        fn test(vector: Vector2, set_to: Vector2) {
+            let api = crate::get_api();
 
-        assert_eq!(vector, vector_from_variant);
+            let copied = vector;
+            unsafe {
+                assert_eq!(vector.x, (api.godot_vector2_get_x)(&copied as *const _ as *const sys::godot_vector2));
+                assert_eq!(vector.y, (api.godot_vector2_get_y)(&copied as *const _ as *const sys::godot_vector2));
+            }
+            assert_eq!(vector, copied);
+
+            let mut copied = vector;
+            unsafe {
+                (api.godot_vector2_set_x)(&mut copied as *mut _ as *mut sys::godot_vector2, set_to.x);
+                (api.godot_vector2_set_y)(&mut copied as *mut _ as *mut sys::godot_vector2, set_to.y);
+            }
+            assert_eq!(set_to, copied);
+
+            let variant = vector.to_variant();
+            let vector_from_variant = Vector2::from_variant(&variant).unwrap();
+            assert_eq!(vector, vector_from_variant);
+        }
+
+        test(Vector2::new(1.0, 2.0), Vector2::new(3.0, 4.0));
+        test(Vector2::new(3.0, 4.0), Vector2::new(5.0, 6.0));
     }
     );
 
 #[cfg(test)]
 mod tests {
     use super::Vector2;
+
+    #[test]
+    fn it_is_copy() {
+        fn copy<T: Copy>() {}
+        copy::<Vector2>();
+    }
+
+    #[test]
+    fn it_has_the_same_size() {
+        use std::mem::size_of;
+        assert_eq!(size_of::<sys::godot_vector2>(), size_of::<Vector2>());
+    }
 
     #[test]
     fn it_supports_equality() {
--- gdnative-core/src/vector3.rs
+++ gdnative-core/src/vector3.rs
@@ -17,17 +17,50 @@ impl FromVariant for Vector3 {
 
 godot_test!(
     test_vector3_variants {
-        let vector = Vector3::new(1.0, 2.0, 3.0);
-        let variant = vector.to_variant();
-        let vector_from_variant = Vector3::from_variant(&variant).unwrap();
+        fn test(vector: Vector3, set_to: Vector3) {
+            let api = crate::get_api();
 
-        assert_eq!(vector, vector_from_variant);
+            let copied = vector;
+            unsafe {
+                assert_eq!(vector.x, (api.godot_vector3_get_axis)(&copied as *const _ as *const sys::godot_vector3, crate::Vector3Axis::X as u32));
+                assert_eq!(vector.y, (api.godot_vector3_get_axis)(&copied as *const _ as *const sys::godot_vector3, crate::Vector3Axis::Y as u32));
+                assert_eq!(vector.z, (api.godot_vector3_get_axis)(&copied as *const _ as *const sys::godot_vector3, crate::Vector3Axis::Z as u32));
+            }
+            assert_eq!(vector, copied);
+
+            let mut copied = vector;
+            unsafe {
+                (api.godot_vector3_set_axis)(&mut copied as *mut _ as *mut sys::godot_vector3, crate::Vector3Axis::X as u32, set_to.x);
+                (api.godot_vector3_set_axis)(&mut copied as *mut _ as *mut sys::godot_vector3, crate::Vector3Axis::Y as u32, set_to.y);
+                (api.godot_vector3_set_axis)(&mut copied as *mut _ as *mut sys::godot_vector3, crate::Vector3Axis::Z as u32, set_to.z);
+            }
+            assert_eq!(set_to, copied);
+
+            let variant = vector.to_variant();
+            let vector_from_variant = Vector3::from_variant(&variant).unwrap();
+            assert_eq!(vector, vector_from_variant);
+        }
+
+        test(Vector3::new(1.0, 2.0, 3.0), Vector3::new(4.0, 5.0, 6.0));
+        test(Vector3::new(4.0, 5.0, 6.0), Vector3::new(7.0, 8.0, 9.0));
     }
     );
 
 #[cfg(test)]
 mod tests {
     use super::Vector3;
+
+    #[test]
+    fn it_is_copy() {
+        fn copy<T: Copy>() {}
+        copy::<Vector3>();
+    }
+
+    #[test]
+    fn it_has_the_same_size() {
+        use std::mem::size_of;
+        assert_eq!(size_of::<sys::godot_vector3>(), size_of::<Vector3>());
+    }
 
     #[test]
     fn it_supports_equality() {
