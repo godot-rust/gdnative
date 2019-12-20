bcc695192737c37d5dd33f175d512f427859037e
Archina Void <archina5thrider@gmail.com>
2019-12-20 21:13:50 +0100

    squashed impl properties macro
    add default value
    
    add builder_helper through block
    
    fix formating
    
    drop unnecessary clone
    
    Revert "formating on bindings_generator"
    
    This reverts commit 8de9b7183a231b309cb249d823a0b90405b5d4d6.
    rename builder to register_with, use bareFnArgs to extract function, call function
    
    
    use AttributeArgs instead of BareFnArgs
    
    
    improve checks against attribute ident paths
    
    
    rename builder to register_callback, use path instead of ident
    
    
    simplify register_callback assignment
    
    
    map fields in hashMap with map of potential literals
    
    
    forward ident to hashMap, evaluate optional default
    
    
    rename symbol
    
    
    use propertyConfig struct, use filter_map
    
    
    fix formating
    
    
    use suggested changes
    
    
    reapply fmt
    
    
    remove missed comment
    
    
    remove now obsolete function

--- gdnative-derive/src/derive_macro.rs
+++ gdnative-derive/src/derive_macro.rs
@@ -1,10 +1,16 @@
 use proc_macro::TokenStream;
-use syn::{Data, DeriveInput, Fields, Ident, Type};
+use std::collections::HashMap;
+use syn::{Data, DeriveInput, Fields, Ident, Meta, MetaList, NestedMeta, Path, Type};
+
+mod property_args;
+use property_args::{PropertyAttrArgs, PropertyAttrArgsBuilder};
 
 pub(crate) struct DeriveData {
     pub(crate) name: Ident,
     pub(crate) base: Type,
+    pub(crate) register_callback: Option<Path>,
     pub(crate) user_data: Type,
+    pub(crate) properties: HashMap<Ident, PropertyAttrArgs>,
 }
 
 pub(crate) fn parse_derive_input(input: TokenStream) -> DeriveData {
@@ -20,17 +26,26 @@ pub(crate) fn parse_derive_input(input: TokenStream) -> DeriveData {
     let inherit_attr = input
         .attrs
         .iter()
-        .find(|a| a.path.segments[0].ident == "inherit")
+        .find(|a| a.path.is_ident("inherit"))
         .expect("No \"inherit\" attribute found");
 
     // read base class
     let base = syn::parse::<Type>(inherit_attr.tokens.clone().into())
         .expect("`inherits` attribute requires the base type as an argument.");
 
+    let register_callback = input
+        .attrs
+        .iter()
+        .find(|a| a.path.is_ident("register_with"))
+        .map(|attr| {
+            attr.parse_args::<Path>()
+                .expect("`register_with` attributes requires a function as an argument.")
+        });
+
     let user_data = input
         .attrs
         .iter()
-        .find(|a| a.path.segments[0].ident == "user_data")
+        .find(|a| a.path.is_ident("user_data"))
         .map(|attr| {
             syn::parse::<Type>(attr.tokens.clone().into())
                 .expect("`userdata` attribute requires a type as an argument.")
@@ -48,13 +63,48 @@ pub(crate) fn parse_derive_input(input: TokenStream) -> DeriveData {
     };
 
     // read exported properties
-    if let Fields::Named(_names) = struct_data.fields {
-        // TODO
-    }
+    let properties = if let Fields::Named(names) = &struct_data.fields {
+        names
+            .named
+            .iter()
+            .filter_map(|field| {
+                let mut property_args = None;
+
+                for attr in field.attrs.iter() {
+                    if !attr.path.is_ident("property") {
+                        continue;
+                    }
+
+                    let meta = attr
+                        .parse_meta()
+                        .expect("should be able to parse attribute arguments");
+                    if let Meta::List(MetaList { nested, .. }) = meta {
+                        property_args
+                            .get_or_insert_with(PropertyAttrArgsBuilder::default)
+                            .extend(nested.iter().map(|arg| match arg {
+                                NestedMeta::Meta(Meta::NameValue(ref pair)) => pair,
+                                _ => panic!("unexpected argument: {:?}", arg),
+                            }));
+                    } else {
+                        panic!("unexpected meta variant: {:?}", meta);
+                    }
+                }
+
+                property_args.map(|builder| {
+                    let ident = field.ident.clone().expect("fields should be named");
+                    (ident, builder.done())
+                })
+            })
+            .collect::<HashMap<_, _>>()
+    } else {
+        HashMap::new()
+    };
 
     DeriveData {
         name: ident,
         base,
+        register_callback,
         user_data,
+        properties,
     }
 }
