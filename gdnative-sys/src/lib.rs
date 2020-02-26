fcd711b8eebd7c5d5e0283f177402c328f65f85b
stewbasic <stewbasic@google.com>
2020-02-27 06:33:50 +1100

    Report initialization errors to Godot.
    Use the callbacks report_version_mismatch and report_loading_error provided by
    Godot, instead of panicking. After reporting errors, Godot still calls
    godot_nativescript_init and godot_gdnative_terminate, so we must exit early from
    those.

--- gdnative-sys/src/lib.rs
+++ gdnative-sys/src/lib.rs
@@ -7,22 +7,45 @@
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 include!(concat!(env!("OUT_DIR"), "/api_wrapper.rs"));
 
+pub enum InitError {
+    VersionMismatch {
+        api_type: GDNATIVE_API_TYPES,
+        want: godot_gdnative_api_version,
+        got: godot_gdnative_api_version,
+    },
+    Generic {
+        message: String,
+    },
+}
+
+fn map_option_to_init_error<T>(t: Option<T>, message: &'static str) -> Result<T, InitError> {
+    match t {
+        Some(t) => Ok(t),
+        None => Err(InitError::Generic {
+            message: message.to_string(),
+        }),
+    }
+}
+
 unsafe fn find_api_ptr(
     core_api: *const godot_gdnative_core_api_struct,
     api_type: GDNATIVE_API_TYPES,
     version_major: u32,
     version_minor: u32,
-) -> *const godot_gdnative_api_struct {
+) -> Result<*const godot_gdnative_api_struct, InitError> {
+    let mut got = None;
     let mut api = core_api as *const godot_gdnative_api_struct;
     if (*api).type_ as u32 == api_type as u32 {
         while !api.is_null() {
-            if (*api).version.minor == version_minor
-                && (*api).version.major == version_major
-                // The boolean expression below SHOULD always be true;
-                // we will double check to be safe.
-                && (*api).type_ as u32 == api_type as u32
-            {
-                return api;
+            // The boolean expression below SHOULD always be true;
+            // we will double check to be safe.
+            if (*api).type_ as u32 == api_type as u32 {
+                let (major, minor) = ((*api).version.major, (*api).version.minor);
+                if major == version_major && minor == version_minor {
+                    return Ok(api);
+                } else {
+                    got = Some(godot_gdnative_api_version { major, minor });
+                }
             }
             api = (*api).next;
         }
@@ -32,21 +55,31 @@ unsafe fn find_api_ptr(
             *(*core_api).extensions.offset(i as _) as *const godot_gdnative_api_struct;
         if (*extension).type_ as u32 == api_type as u32 {
             while !extension.is_null() {
-                if (*extension).version.minor == version_minor
-                    && (*extension).version.major == version_major
-                    // The boolean expression below SHOULD always be true;
-                    // we will double check to be safe.
-                    && (*extension).type_ as u32 == api_type as u32
-                {
-                    return extension;
+                // The boolean expression below SHOULD always be true;
+                // we will double check to be safe.
+                if (*extension).type_ as u32 == api_type as u32 {
+                    let (major, minor) = ((*extension).version.major, (*extension).version.minor);
+                    if major == version_major && minor == version_minor {
+                        return Ok(extension);
+                    } else {
+                        got = Some(godot_gdnative_api_version { major, minor });
+                    }
                 }
                 extension = (*extension).next;
             }
         }
     }
-    panic!(
-        "Couldn't find API struct with type {type}, version {version:?}",
-        type = api_type,
-        version = (version_major, version_minor),
-    );
+    match got {
+        Some(got) => Err(InitError::VersionMismatch {
+            want: godot_gdnative_api_version {
+                major: version_major,
+                minor: version_minor,
+            },
+            got,
+            api_type,
+        }),
+        None => Err(InitError::Generic {
+            message: format!("Couldn't find API struct with type {}", api_type),
+        }),
+    }
 }
