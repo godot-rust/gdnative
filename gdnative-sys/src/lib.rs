6cffda68bfbf6b5d26c0d54414e4e2b214d85847
stewbasic <stewbasic@google.com>
2020-03-02 18:32:57 +1100

    Refactor: extract repeated code in find_api_ptr.

--- gdnative-sys/src/lib.rs
+++ gdnative-sys/src/lib.rs
@@ -27,14 +27,13 @@ fn map_option_to_init_error<T>(t: Option<T>, message: &'static str) -> Result<T,
     }
 }
 
-unsafe fn find_api_ptr(
-    core_api: *const godot_gdnative_core_api_struct,
+unsafe fn find_version(
+    mut api: *const godot_gdnative_api_struct,
     api_type: GDNATIVE_API_TYPES,
     version_major: u32,
     version_minor: u32,
-) -> Result<*const godot_gdnative_api_struct, InitError> {
+) -> Option<Result<*const godot_gdnative_api_struct, InitError>> {
     let mut got = None;
-    let mut api = core_api as *const godot_gdnative_api_struct;
     if (*api).type_ as u32 == api_type as u32 {
         while !api.is_null() {
             // The boolean expression below SHOULD always be true;
@@ -42,7 +41,7 @@ unsafe fn find_api_ptr(
             if (*api).type_ as u32 == api_type as u32 {
                 let (major, minor) = ((*api).version.major, (*api).version.minor);
                 if major == version_major && minor == version_minor {
-                    return Ok(api);
+                    return Some(Ok(api));
                 } else {
                     got = Some(godot_gdnative_api_version { major, minor });
                 }
@@ -50,36 +49,56 @@ unsafe fn find_api_ptr(
             api = (*api).next;
         }
     }
-    for i in 0..(*core_api).num_extensions {
-        let mut extension =
-            *(*core_api).extensions.offset(i as _) as *const godot_gdnative_api_struct;
-        if (*extension).type_ as u32 == api_type as u32 {
-            while !extension.is_null() {
-                // The boolean expression below SHOULD always be true;
-                // we will double check to be safe.
-                if (*extension).type_ as u32 == api_type as u32 {
-                    let (major, minor) = ((*extension).version.major, (*extension).version.minor);
-                    if major == version_major && minor == version_minor {
-                        return Ok(extension);
-                    } else {
-                        got = Some(godot_gdnative_api_version { major, minor });
-                    }
-                }
-                extension = (*extension).next;
-            }
-        }
-    }
-    match got {
-        Some(got) => Err(InitError::VersionMismatch {
+    got.map(|got| {
+        Err(InitError::VersionMismatch {
             want: godot_gdnative_api_version {
                 major: version_major,
                 minor: version_minor,
             },
             got,
             api_type,
-        }),
-        None => Err(InitError::Generic {
-            message: format!("Couldn't find API struct with type {}", api_type),
-        }),
+        })
+    })
+}
+
+unsafe fn find_api_ptr(
+    core_api: *const godot_gdnative_core_api_struct,
+    api_type: GDNATIVE_API_TYPES,
+    version_major: u32,
+    version_minor: u32,
+) -> Result<*const godot_gdnative_api_struct, InitError> {
+    let mut last_error = None;
+    match find_version(
+        core_api as *const godot_gdnative_api_struct,
+        api_type,
+        version_major,
+        version_minor,
+    ) {
+        Some(Ok(api)) => {
+            return Ok(api);
+        }
+        Some(Err(error)) => {
+            last_error = Some(error);
+        }
+        None => {}
     }
+    for i in 0..(*core_api).num_extensions {
+        match find_version(
+            *(*core_api).extensions.offset(i as _),
+            api_type,
+            version_major,
+            version_minor,
+        ) {
+            Some(Ok(api)) => {
+                return Ok(api);
+            }
+            Some(Err(error)) => {
+                last_error = Some(error);
+            }
+            None => {}
+        }
+    }
+    Err(last_error.unwrap_or(InitError::Generic {
+        message: format!("Couldn't find API struct with type {}", api_type),
+    }))
 }
