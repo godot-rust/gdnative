fb2b8d3bd8185a9fea92486d5737ae8f8ef48980
Justin Wood <me@ankhers.dev>
2020-03-20 16:53:19 -0400

    Write a macro to automatically implement the NodeExt trait

--- examples/dodge_the_creeps/src/extensions.rs
+++ examples/dodge_the_creeps/src/extensions.rs
@@ -4,26 +4,20 @@ pub trait NodeExt {
     unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(&self, path: P) -> Option<T>;
 }
 
-impl NodeExt for Node {
-    unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(&self, path: P) -> Option<T> {
-        self.get_node(path.into()).and_then(|node| node.cast())
-    }
+macro_rules! impl_node_ext_for {
+    ($t:ty) => {
+        impl NodeExt for $t {
+            unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(
+                &self,
+                path: P,
+            ) -> Option<T> {
+                self.get_node(path.into()).and_then(|node| node.cast())
+            }
+        }
+    };
 }
 
-impl NodeExt for RigidBody2D {
-    unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(&self, path: P) -> Option<T> {
-        self.get_node(path.into()).and_then(|node| node.cast())
-    }
-}
-
-impl NodeExt for CanvasLayer {
-    unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(&self, path: P) -> Option<T> {
-        self.get_node(path.into()).and_then(|node| node.cast())
-    }
-}
-
-impl NodeExt for Area2D {
-    unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(&self, path: P) -> Option<T> {
-        self.get_node(path.into()).and_then(|node| node.cast())
-    }
-}
+impl_node_ext_for!(Node);
+impl_node_ext_for!(RigidBody2D);
+impl_node_ext_for!(CanvasLayer);
+impl_node_ext_for!(Area2D);
