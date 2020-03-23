28da639b807325fffb119a6833638575c068948f
Justin Wood <me@ankhers.dev>
2020-03-23 09:32:23 -0400

    Remove unnecessary implementations of the NodeExt trait

--- examples/dodge_the_creeps/src/extensions.rs
+++ examples/dodge_the_creeps/src/extensions.rs
@@ -4,20 +4,8 @@ pub trait NodeExt {
     unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(&self, path: P) -> Option<T>;
 }
 
-macro_rules! impl_node_ext_for {
-    ($t:ty) => {
-        impl NodeExt for $t {
-            unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(
-                &self,
-                path: P,
-            ) -> Option<T> {
-                self.get_node(path.into()).and_then(|node| node.cast())
-            }
-        }
-    };
+impl NodeExt for Node {
+    unsafe fn get_typed_node<T: GodotObject, P: Into<NodePath>>(&self, path: P) -> Option<T> {
+        self.get_node(path.into()).and_then(|node| node.cast())
+    }
 }
-
-impl_node_ext_for!(Node);
-impl_node_ext_for!(RigidBody2D);
-impl_node_ext_for!(CanvasLayer);
-impl_node_ext_for!(Area2D);
