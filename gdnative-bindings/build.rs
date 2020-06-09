b74dcd85eca7b2445ac0d9599007b016d64c2164
Benjamin Halsted <bg@hlstd.com>
2020-06-09 15:48:41 -0700

    Adding BufWriter to gdnative-bindings/build.rs
    This reduces the build time on my box by close to a second. 
    Figure it’d be even better on boxes that may have worse iops.

--- gdnative-bindings/build.rs
+++ gdnative-bindings/build.rs
@@ -2,13 +2,17 @@ use gdnative_bindings_generator::*;
 
 use std::env;
 use std::fs::File;
+use std::io::BufWriter;
 use std::path::PathBuf;
 
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let mut types_output = File::create(out_path.join("bindings_types.rs")).unwrap();
-    let mut traits_output = File::create(out_path.join("bindings_traits.rs")).unwrap();
-    let mut methods_output = File::create(out_path.join("bindings_methods.rs")).unwrap();
+    let mut types_output =
+        BufWriter::new(File::create(out_path.join("bindings_types.rs")).unwrap());
+    let mut traits_output =
+        BufWriter::new(File::create(out_path.join("bindings_traits.rs")).unwrap());
+    let mut methods_output =
+        BufWriter::new(File::create(out_path.join("bindings_methods.rs")).unwrap());
 
     // gdnative-core already implements all dependencies of Object
     let to_ignore = { strongly_connected_components(&Api::new(), "Object", None) };
