88d1e606a9bf5d457cfbce5dcdd1acddba15dcac
toasteater <48371905+toasteater@users.noreply.github.com>
2020-06-23 06:00:32 +0000

    Use OwnedToVariant for return values.

--- gdnative-core/src/nativescript/macros.rs
+++ gdnative-core/src/nativescript/macros.rs
@@ -188,7 +188,7 @@ macro_rules! godot_wrap_method_inner {
                                 $($pname,)*
                                 $($opt_pname,)*
                             );
-                            <$retty as $crate::ToVariant>::to_variant(&ret)
+                            <$retty as $crate::OwnedToVariant>::owned_to_variant(ret)
                         })
                         .unwrap_or_else(|err| {
                             $crate::godot_error!("gdnative-core: method call failed with error: {:?}", err);
