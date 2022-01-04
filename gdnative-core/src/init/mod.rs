f252d576ddb7534229e1661961613e800ef3c392
Jan Haller <bromeon@gmail.com>
2022-01-04 21:01:02 +0100

    Minor doc (mod export/init)

--- gdnative-core/src/init/mod.rs
+++ gdnative-core/src/init/mod.rs
@@ -1,4 +1,40 @@
 //! Global initialization and termination of the library.
+//!
+//! This module provides all the plumbing required for global initialization and shutdown of godot-rust.
+//!
+//! ## Init and exit hooks
+//!
+//! Three endpoints are automatically invoked by the engine during startup and shutdown:
+//!
+//! * [`godot_gdnative_init`],
+//! * [`godot_nativescript_init`],
+//! * [`godot_gdnative_terminate`],
+//!
+//! All three must be present. To quickly define all three endpoints using the default names,
+//! use [`godot_init`].
+//!
+//! ## Registering script classes
+//!
+//! [`InitHandle`] is the registry of all your exported symbols.
+//! To register script classes, call [`InitHandle::add_class()`] or [`InitHandle::add_tool_class()`]
+//! in your `godot_nativescript_init` or `godot_init` callback:
+//!
+//! ```no_run
+//! use gdnative::prelude::*;
+//!
+//! #[derive(NativeClass)]
+//! # #[no_constructor]
+//! struct HelloWorld { /* ... */ }
+//!
+//! #[methods]
+//! impl HelloWorld { /* ... */ }
+//!
+//! fn init(handle: InitHandle) {
+//!     handle.add_class::<HelloWorld>();
+//! }
+//!
+//! godot_init!(init);
+//! ```
 
 mod info;
 mod init_handle;
