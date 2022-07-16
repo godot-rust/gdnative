c111343f75bdb83dbcc38dee749531e414597785
Jan Haller <bromeon@gmail.com>
2022-07-16 23:11:13 +0200

    Print properties in both Rust and GDScript, to highlight ordering discrepancies

---
+++ examples/property-export/GDScriptPrinter.gd
@@ -0,0 +1,22 @@
+extends Node
+
+func _ready():
+	var rust = get_node("../PropertyExport")
+
+	print("\n-----------------------------------------------------------------")
+	print("Print from GDScript (note the deterministically ordered map/set):")
+	print("  Vec (name):");
+	for name in rust.name_vec:
+		print("  * %s" % name)
+
+	print("\n  HashMap (string -> color):")
+	for string in rust.color_map:
+		var color = rust.color_map[string]
+		print("  * %s -> #%s" % [string, color.to_html(false)]);
+
+	print("\n  HashSet (ID):")
+	for id in rust.id_set:
+		print("  * %s" % id)
+
+	# The program has printed the contents and fulfilled its purpose, quit
+	get_tree().quit()
--- examples/property-export/src/lib.rs
+++ examples/property-export/src/lib.rs
@@ -22,25 +22,23 @@ impl PropertyExport {
     }
 
     #[export]
-    fn _ready(&self, base: &Node) {
-        godot_print!("Vec (name):");
+    fn _ready(&self, _base: &Node) {
+        godot_print!("------------------------------------------------------------------");
+        godot_print!("Print from Rust (note the unordered map/set):");
+        godot_print!("  Vec (name):");
         for name in &self.name_vec {
-            godot_print!("* {}", name);
+            godot_print!("  * {}", name);
         }
 
-        godot_print!("\nHashMap (string -> color):");
+        godot_print!("\n  HashMap (string -> color):");
         for (string, color) in &self.color_map {
-            godot_print!("* {} -> #{}", string, color.to_html(false));
+            godot_print!("  * {} -> #{}", string, color.to_html(false));
         }
 
-        godot_print!("\nHashSet (ID):");
+        godot_print!("\n  HashSet (ID):");
         for id in &self.id_set {
-            godot_print!("* {}", id);
+            godot_print!("  * {}", id);
         }
-
-        // The program has printed the contents and fulfilled its purpose, quit
-        let scene_tree = base.get_tree().unwrap();
-        unsafe { scene_tree.assume_safe() }.quit(0);
     }
 }
 
