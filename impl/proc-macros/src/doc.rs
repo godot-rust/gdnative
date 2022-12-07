398b57adbcfb645f7f048a2e27bec8a6c2688881
Chitose Yuuzaki <chitoyuu@potatoes.gay>
2022-12-07 07:05:50 +0000

    Removed TODOs and code that didn't turn out to be necessary

--- impl/proc-macros/src/doc.rs
+++ impl/proc-macros/src/doc.rs
@@ -1,42 +1,6 @@
 use syn::visit_mut::VisitMut;
 use syn::{Attribute, ItemFn, ItemImpl};
 
-/*
-Leaving code commented-out, as this might be very useful elsewhere
-
-use proc_macro2::TokenStream;
-use quote::ToTokens;
-use syn::{Item};
-pub fn variant_collection_safety(
-    _attr: proc_macro::TokenStream,
-    item: proc_macro::TokenStream,
-) -> Result<TokenStream, syn::Error> {
-    let mut item = syn::parse::<Item>(item)?;
-    let mut visit = IncludeDocs {
-        docs: &[
-            "# Safety",
-            "",
-            "Generally, it's not recommended to mutate variant collections that may be shared. Prefer",
-            "`ThreadLocal` or `Unique` collections instead. If you're sure that the current reference",
-            "is unique, you may use [`assume_unique`](#method.assume_unique) to convert it to a `Unique`",
-            "collection. You may subsequently use [`into_thread_local`](#method.into_thread_local) to",
-            "convert it to a `ThreadLocal` one.",
-            "",
-            "It is only safe to perform operations that may allocate on a shared collection when no",
-            "other thread may access the underlying collection during the call.",
-        ],
-        deprecated: Some(concat!(
-            "Care should be used when mutating shared variant collections. Prefer `ThreadLocal` ",
-            "or `Unique` collections unless you're absolutely sure that you want this. ",
-            "You may use [assume_unique](#method.assume_unique) to convert this to a `Unique` ",
-            "collection if you are sure that this is in fact the only reference."
-        )),
-    };
-    visit.visit_item_mut(&mut item);
-    Ok(item.to_token_stream())
-}
-*/
-
 struct IncludeDocs<'a> {
     docs: &'a [&'a str],
     deprecated: Option<&'a str>,
--- impl/proc-macros/src/lib.rs
+++ impl/proc-macros/src/lib.rs
@@ -24,17 +24,6 @@ pub fn decl_typed_array_element(input: TokenStream) -> TokenStream {
         .into()
 }
 
-/*
-Leaving code commented-out, as this might be very useful elsewhere
-
-#[proc_macro_attribute]
-pub fn doc_variant_collection_safety(attr: TokenStream, item: TokenStream) -> TokenStream {
-    self::doc::variant_collection_safety(attr, item)
-        .unwrap_or_else(to_compile_errors)
-        .into()
-}
-*/
-
 fn to_compile_errors(error: syn::Error) -> proc_macro2::TokenStream {
     let compile_error = error.to_compile_error();
     quote!(#compile_error)
