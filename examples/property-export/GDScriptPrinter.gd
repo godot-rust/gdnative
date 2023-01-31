fa43b854a4336891d072766a9886d0e1ce7546f2
Chitose Yuuzaki <chitoyuu@potatoes.gay>
2023-01-31 08:29:45 +0000

    Remove Export impls with surprising behavior
    Also added documentation on the rationales behind leaving `Export`
    unimplemented for standard Rust collections, pointing to alternatives.
    
    Close #1009

--- examples/property-export/GDScriptPrinter.gd
+++ examples/property-export/GDScriptPrinter.gd
@@ -4,17 +4,17 @@ func _ready():
 	var rust = get_node("../PropertyExport")
 
 	print("\n-----------------------------------------------------------------")
-	print("Print from GDScript (note the lexicographically ordered map/set):")
-	print("  Vec (name):");
+	print("Print from GDScript:")
+	print("  PoolArray<GodotString>:");
 	for name in rust.name_vec:
 		print("  * %s" % name)
 
-	print("\n  HashMap (string -> color):")
+	print("\n  Dictionary (string -> color):")
 	for string in rust.color_map:
 		var color = rust.color_map[string]
 		print("  * %s -> #%s" % [string, color.to_html(false)]);
 
-	print("\n  HashSet (ID):")
+	print("\n  PoolArray<i32>:")
 	for id in rust.id_set:
 		print("  * %s" % id)
 
