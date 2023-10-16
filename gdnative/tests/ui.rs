348bac2d53e3cf2e200f064d89de296718cb43b2
bogay <pojay11523@gmail.com>
2023-10-17 01:10:57 +0800

    test(ui): use glob to match tests for #[derive(Export)]

--- gdnative/tests/ui.rs
+++ gdnative/tests/ui.rs
@@ -43,15 +43,7 @@ fn ui_tests() {
 
     // Export
     t.pass("tests/ui/export_pass.rs");
-    t.compile_fail("tests/ui/export_fail_01.rs");
-    t.compile_fail("tests/ui/export_fail_02.rs");
-    t.compile_fail("tests/ui/export_fail_03.rs");
-    t.compile_fail("tests/ui/export_fail_04.rs");
-    t.compile_fail("tests/ui/export_fail_05.rs");
-    t.compile_fail("tests/ui/export_fail_06.rs");
-    t.compile_fail("tests/ui/export_fail_07.rs");
-    t.compile_fail("tests/ui/export_fail_08.rs");
-    t.compile_fail("tests/ui/export_fail_09.rs");
+    t.compile_fail("tests/ui/export_fail_*.rs");
 }
 
 // FIXME(rust/issues/54725): Full path spans are only available on nightly as of now
